54d415eef9201c093cd835a9c8f289f62cf2a19c
Fabian Herschel <fabian.herschel@suse.com>
2023-05-26 12:11:58 +0200

    angi: tools: saphana_sr_tools.py - argument parsing; argument "format" (table, path, json)

--- test/saphana_sr_tools.py
+++ test/saphana_sr_tools.py
@@ -48,6 +48,7 @@ class HanaCluster():
         self.selection = 'default'
         self.config = {}
         self.config['cib_file'] = "-"
+        self.config['format'] = "table"
 
     def xml_import(self, filename):
         self.tree = ET.parse(filename)
@@ -232,18 +233,22 @@ if __name__ == "__main__":
     #                    action="store_true")
     args = parser.parse_args()
     if args.cibfile:
-        #self.message("PARAM: testFile: {}".format(args.testFile))
         myCluster.config['cib_file'] = args.cibfile
+    if args.format:
+        myCluster.config['format'] = args.format
     myCluster.xml_import(myCluster.config['cib_file'])
     myCluster.fill_global_dict()
     myCluster.fill_site_dict()
     myCluster.fill_host_dict()
-    myCluster.print_dic_as_table(myCluster.global_dict,"Global")
-    myCluster.print_dic_as_table(myCluster.site_dict,"Site")
-    myCluster.print_dic_as_table(myCluster.host_dict,"Host")
-    myCluster.print_all_as_json()
+    if myCluster.config['format'] == "table":
+        myCluster.print_dic_as_table(myCluster.global_dict,"Global")
+        myCluster.print_dic_as_table(myCluster.site_dict,"Site")
+        myCluster.print_dic_as_table(myCluster.host_dict,"Host")
+    elif myCluster.config['format'] == "json":
+        myCluster.print_all_as_json()
+    elif myCluster.config['format'] == "path":
+        myCluster.print_dic_as_path(myCluster.host_dict,"Host", quote='"')
     #myCluster.print_dic_as_json(myCluster.host_dict,"Host")
-    #myCluster.print_dic_as_path(myCluster.host_dict,"Host", quote='"')
 
 
 """
