0147af1ee2d4e1132324652824d5ab78a8f56d7f
Fabian Herschel <fabian.herschel@suse.com>
2023-08-11 13:59:32 +0200

    angi: package: tools - "pylinter" all python scripts/libs in tools

--- tools/saphana_sr_tools.py
+++ tools/saphana_sr_tools.py
@@ -9,7 +9,6 @@
 # pylint: disable=global-variable-undefined
 # pylint: disable=too-few-public-methods
 # pylint: disable=consider-using-get
-# pylint: disable=no-self-use
 # flake8-in-file-ignores: noqa: E501
 """
  saphana_sr_tools.py
