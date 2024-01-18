39be1a0190687aed1c5547498e244dc40edf9bc6
Fabian Herschel <fabian.herschel@suse.com>
2024-01-18 12:44:09 +0100

    angi: susHanaSr.py - removing fallback-file if a new event comes - in (see also bsc1215693 (was about SAPHanaSR-ScaleOut)

--- srHook/susHanaSR.py
+++ srHook/susHanaSR.py
@@ -91,7 +91,15 @@ try:
                 self.tracer.info(f"{self.__class__.__name__}.{method}() {my_msg}\n")
                 fallback_file_name = f"../.crm_attribute.{my_site}"
                 fallback_stage_file_name = f"../.crm_attribute.stage.{my_site}"
-                if ret_code != 0:
+                if ret_code == 0:
+                    # 
+                    # cluster attribute set was successfull - delete pending fallback file, if existing
+                    try:
+                        os.remove(fallback_file_name)
+                        self.tracer.info("new event - pending fallback file {0} deleted".format(fallback_file_name))
+                    except FileNotFoundError:
+                        pass
+                else:
                     #
                     # FALLBACK
                     # sending attribute to the cluster failed - using fallback method and write
