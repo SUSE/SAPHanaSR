9b877d1c099efe4b86d0f815b108cb6a3bc0a7be
lpinne <lars.pinne@suse.com>
2025-03-06 14:01:24 +0100

    tester: SAPHanaSR-tests-angi-ScaleOut.7 SAPHanaSR-tests-description.7 runtests_angi-ScaleOut_conservative.txt saphana_sr_test.py standby_prim_worker_node.json

--- test/tester/saphana_sr_test.py
+++ test/tester/saphana_sr_test.py
@@ -807,6 +807,10 @@ class SaphanasrTest:
             cmd = "crm node standby {}".format(self.topolo['pHost'])
         elif action_name == "opn":
             cmd = "crm node online {}".format(self.topolo['pHost'])
+        elif action_name == "standby_prim_worker_node":
+            cmd = "crm node standby {}".format(self.topolo['pWorker'])
+        elif action_name == "online_prim_worker_node":
+            cmd = "crm node online {}".format(self.topolo['pWorker'])
         elif action_name == "standby_secn_worker_node":
             cmd = "crm node standby {}".format(self.topolo['sWorker'])
         elif action_name == "online_secn_worker_node":
@@ -858,7 +862,7 @@ class SaphanasrTest:
             action_rc = 0
         elif action_name_short in ("kill_prim_inst", "kill_prim_worker_inst", "kill_secn_inst", "kill_secn_worker_inst", "kill_prim_indexserver", "kill_secn_indexserver", "kill_prim_worker_indexserver", "kill_secn_worker_indexserver", "kill_prim_nameserver", "kill_secn_nameserver", "kill_prim_xsengine", "kill_secn_xsengine", "bmt"):
             action_rc = self.action_on_hana(action_name)
-        elif action_name_short in ("ssn", "osn", "spn", "opn", "cleanup", "kill_secn_node", "kill_secn_worker_node", "kill_prim_node", "kill_prim_worker_node", "simulate_split_brain","standby_secn_worker_node", "online_secn_worker_node"):
+        elif action_name_short in ("ssn", "osn", "spn", "opn", "cleanup", "kill_secn_node", "kill_secn_worker_node", "kill_prim_node", "kill_prim_worker_node", "simulate_split_brain", "standby_prim_worker_node", "online_prim_worker_node", "standby_secn_worker_node", "online_secn_worker_node"):
             action_rc = self.action_on_cluster(action_name)
         elif action_name_short in ("sleep", "shell"):
             action_rc = self.action_on_os(action_name)
